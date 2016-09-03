8097f6a364086467e99865cdc2f8751df17ec6de
Steven <banguijun@sina.com>
2016-09-03 17:04:28 +0800

    添加模板
    加入模板base.html，并实现继承

--- mytask/views.py
+++ mytask/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    # return render(request, 'home.html')
-    return HttpResponse("<html><body>This is homepage of this website - My Task.</body></html>")
+    return render(request, 'home.html')
+    # return HttpResponse('home.html')
+    # return HttpResponse("<html><body>This is homepage of this website - My Task.</body></html>")
