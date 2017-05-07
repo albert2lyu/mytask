6d1c0bfd64810dfe40e576f650598e93aa56a494
Steven <banguijun@sina.com>
2017-05-07 16:08:16 +0800

    添加注册功能
    用户可以注册。存在问题：注册后数据未保存，存在非法用户造成同样的注册失效。

--- examiner_auth/views.py
+++ examiner_auth/views.py
@@ -2,11 +2,33 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
-from models import UserProfile
+
+from models import UserProfile, StudyTeam, Grade
 
 def user_register(request) : 
-    return redirect('/')
+    studyteams = StudyTeam.objects.all()
+    grades = Grade.objects.all()
+    
+    user_, user_created_ = User.objects.get_or_create(username = request.POST.get('username', False))
+    if user_created_ : 
+        # already exists
+        return redirect('/accounts/login/')
+    user_.password = request.POST.get('password', False)
+    studyTeam_, team_created_ = StudyTeam.objects.get_or_create(studyTeam = request.POST.get('studyTeam', False))
+    grade_, grade_created_ = Grade.objects.get_or_create(grade = request.POST.get('grade', False))
+ 
+    profileUser = UserProfile.objects.create(user = user_, \
+        nameZh = request.POST.get('nameZh', False), \
+        examinationCode = request.POST.get('examinationCode', False), \
+        studyTeam = studyTeam_, \
+        grade = grade_)
+    user_.save()
+    profileUser.save()
+    return render(request, 'register.html', \
+        {'studyteams' : studyteams, \
+        'grades' : grades})
 
 def user_logout(request) : 
     logout(request)
